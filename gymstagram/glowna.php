a195fc01d468d7e602f571127118d1483e8ab646
wktrmtsk <166541464+wktrmtsk@users.noreply.github.com>
2024-06-09 12:56:40 +0200

    Update glowna.php

--- gymstagram/glowna.php
+++ gymstagram/glowna.php
@@ -13,7 +13,7 @@
       <form class="search-box">
         <input type="text" placeholder="Szukaj..." />
         <button type="submit" class="btn">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256">
+          <svg class="search-svg" xmlns="http://www.w3.org/2000/svg" width="13" height="14" fill="#000000" viewBox="0 0 256 256">
             <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
           </svg>
         </button>
@@ -29,15 +29,13 @@
         </svg>    
       </a>
       <div class="dropdown">
-        <a href="#">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256">
-            <path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
-          </svg>
-        </a>
-        <div class="dropdown-content">
-          <a href="#">Wyloguj</a>
+        <button class="dropbtn">&#9776;</button>
+            <div class="dropdown-content">
+                <a href="#">Opcja 1</a>
+                <a href="#">Opcja 2</a>
+                <a href="startowa.html">Wyloguj</a>
+            </div>
         </div>
-      </div>
     </nav>
     <div class="main-content">
       <div class="post-section">
